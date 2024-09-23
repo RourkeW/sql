2a41042b8949891197fb3981a7d7024e9df39375
Rourke Whitcombe <rourke28whit@gmail.com>
2024-09-23 03:07:25 +0000

    feat: I have added some more queries

--- sql.psycopg2.py
+++ sql.psycopg2.py
@@ -4,7 +4,12 @@ connection = psycopg2.connect(database="chinook")
 
 cursor = connection.cursor()
 
-cursor.execute('SELECT * FROM "Artist"')
+#cursor.execute('SELECT * FROM "Artist"')
+#cursor.execute('SELECT "Name" FROM "Artist"')
+#cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+#cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+#cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+#cursor.execute('SELECT * FROM "Track" WHERE "ArtistId" = %s', ['Queen])
 
 results = cursor.fetchall()
 
